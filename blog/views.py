01bf9e140303db4d43bfee646ae005dda69ddf86
zeeyeeteecar <tomtangjob@gmail.com>
2023-04-19 15:40:18 -0700

    1

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+    'author':'CoreyMS',
+   'title':'Blog Post 1',
+   'content':'First post content',
+   'date_posted':'August 27, 2018',
+    },
+    {
+    'author':'Jane Doe',
+   'title':'Blog Post 2',
+   'content':'2nd  post content',
+   'date_posted':'August 27, 2018',
+    },
+]
+
+def home(request):
+    context = {
+        'posts':posts 
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+# Create your views here.
