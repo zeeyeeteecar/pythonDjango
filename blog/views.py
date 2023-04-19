480c3cb8de5c4baac3c03667326c30895d6c24bf
zeeyeeteecar <tomtangjob@gmail.com>
2023-04-19 15:48:26 -0700

    3
    13

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 posts = [
     {
     'author':'CoreyMS',
-   'title':'Blog Post 1',
+   'title':'Blog Post A',
    'content':'First post content',
    'date_posted':'August 27, 2018',
     },
     {
     'author':'Jane Doe',
-   'title':'Blog Post 2',
+   'title':'Blog Post B',
    'content':'2nd  post content',
    'date_posted':'August 27, 2018',
     },
