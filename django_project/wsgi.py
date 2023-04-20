8725273b304e18222e8cb42ff0dd176757fdbff9
zeeyeeteecar <tomtangjob@gmail.com>
2023-04-19 17:48:52 -0700

    4

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
 
